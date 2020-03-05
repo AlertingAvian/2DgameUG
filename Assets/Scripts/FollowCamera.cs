c09c4d98a96e8680acfd91fa523230ae90f1bbaa
Patrick <boyscoutpatrick813@gmail.com>
2020-03-05 14:24:39 -0600

    Camera Follow

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	public float speed = 15f;
+
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start()
+	{
+		targetPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (target)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+
+			interpVelocity = targetDirection.magnitude * speed;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+			transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+		}
+	}
+}
+
+// Original post with image here  >  http://unity3diy.blogspot.com/2015/02/unity-2d-camera-follow-script.html
+
