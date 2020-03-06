fba37994d2d802d65cb1b4a593d34e9b0f13a18e
Patrick <boyscoutpatrick813@gmail.com>
2020-03-06 08:56:31 -0600

    Camera Follow switching update

---
+++ Assets/Scripts/ChangeCamFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class ChangeCamFollow : MonoBehaviour
+{
+    public bool twoPlayer = true;
+    public Follow follow;
+    public FollowCamera oneFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadFile();
+        if (twoPlayer) follow.enabled = true;
+        else if (!twoPlayer) oneFollow.enabled = true;
+    }
+    void LoadFile()
+    {
+        string destination = Application.persistentDataPath + "/settings.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            Debug.LogError("File not found");
+            return;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        bool data = (bool)bf.Deserialize(file);
+        file.Close();
+
+        twoPlayer = data;
+
+        file.Close();
+        Debug.Log(twoPlayer);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
